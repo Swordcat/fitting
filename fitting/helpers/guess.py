f61ae1abea248911701d183a69f06c4c4984070c
Brandur Thorgrimsson <Brandur_thorn@me.com>
2022-07-17 10:07:16 +1000

    Refactored peak guess

--- fitting/helpers/guess.py
+++ fitting/helpers/guess.py
@@ -1,20 +1,20 @@
-import numpy as np
+from numpy import abs, argmax, argmin, array, max, min
 from .filter import smooth
 
 
-def peak(x: np.array, y: np.array) -> list[float]:
-    x, y = filter(x), filter(y)
+def peak(x: array, y: array) -> list[float]:
+    x, y = smooth(x), smooth(y)
     offset = (y[0] + y[-1]) / 2
-    bias = 1 if np.abs(np.min(y) - offset) < np.abs(np.max(y) - offset) else -1
+    bias = 1 if abs(min(y) - offset) < abs(max(y) - offset) else -1
     y = y * bias
-    peak_indx = np.argmax(y)
+    peak_indx = argmax(y)
     height = y[peak_indx]*bias - offset
-    return [height, fwhm(x, y, peak_indx, np.abs(offset), np.abs(height)), offset, x[peak_indx]]
+    return [height, fwhm(x, y, peak_indx, abs(offset), abs(height)), offset, x[peak_indx]]
 
 
-def fwhm(x: np.array, y: np.array, peak_index: int, offset: float, height: float) -> float:
-    try: fwhm_l = x[:peak_index][y[:peak_index] < offset + height / 2][-1]
-    except IndexError: fwhm_l = x[0]
-    try: fwhm_h = x[peak_index:][y[peak_index:] < offset + height / 2][0]
-    except IndexError: fwhm_h = x[-1]
-    return fwhm_h+fwhm_l
+def fwhm(x: array, y: array, peak_index: int, offset: float, height: float) -> float:
+    # gets x value where y value is closest to halfway between background offset and peak height on left side of peak
+    fwhm_l = x[:peak_index][argmin(abs(y[:peak_index] - (height / 2 + offset)))]
+    # gets x value where y value is closest to halfway between background offset and peak height on right side of peak
+    fwhm_r = x[peak_index:][argmin(abs(y[peak_index:] - (height / 2 + offset)))]
+    return abs(fwhm_r-fwhm_l)
